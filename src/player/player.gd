abd0c3d47eec1e9e0a14e1e13573ddbb9df266c4
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-02-22 21:59:16 +0545

    PP-61 Refactor the code

--- src/player/player.gd
+++ src/player/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED: float = 300.0
+const JUMP_VELOCITY: float = -400.0
 
 
 func _physics_process(delta: float) -> void:
@@ -15,7 +15,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis('left', 'right')
+	var direction: float = Input.get_axis('left', 'right')
 	if direction:
 		velocity.x = direction * SPEED
 	else:

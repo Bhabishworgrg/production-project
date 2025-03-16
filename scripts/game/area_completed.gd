13ede6653c496819f372c759df0998e3e84d001c
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-16 12:01:17 +0545

    PP-65 Add warning logs for unexpected body detections

--- scripts/game/area_completed.gd
+++ scripts/game/area_completed.gd
@@ -5,3 +5,5 @@ func _on_player_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
 		const LEVEL_COMPLETE_SCENE: String = 'res://scenes/ui/complete_ui.tscn'
 		get_tree().change_scene_to_file.call_deferred(LEVEL_COMPLETE_SCENE)
+	else:
+		print_rich('[color=yellow]WARNING:[/color] Body not in group player entered AreaCompleted')
--- scripts/game/death_zone.gd
+++ scripts/game/death_zone.gd
@@ -7,3 +7,5 @@ const DEATH_SCENE: Resource = preload('res://scenes/ui/death_screen.tscn')
 func _on_body_entered(body:Node2D) -> void:
 	if body.is_in_group('player'):
 		get_tree().change_scene_to_packed.call_deferred(DEATH_SCENE)
+	else:
+		print('[color=yellow]WARNING:[/color] Body not in group player entered DeathZone')

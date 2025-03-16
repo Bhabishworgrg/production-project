f9765696755ce50ff066780058791798d659c907
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-16 11:57:56 +0545

    PP-65 Make level complete scene immutable

--- scripts/game/area_completed.gd
+++ scripts/game/area_completed.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 func _on_player_entered(body: Node2D) -> void:
 	if body.is_in_group('player'):
-		var level_complete_scene: String = 'res://scenes/ui/complete_ui.tscn'
-		get_tree().change_scene_to_file.call_deferred(level_complete_scene)
+		const LEVEL_COMPLETE_SCENE: String = 'res://scenes/ui/complete_ui.tscn'
+		get_tree().change_scene_to_file.call_deferred(LEVEL_COMPLETE_SCENE)

c4e547f787ea68320736cd284a714b9a0d50d320
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-24 21:14:34 +0545

    PP-22 Generalise background remover for all

--- scripts/img_proc/bg_remover.py
+++ scripts/img_proc/bg_remover.py
@@ -3,7 +3,8 @@ from cv2 import imread, imwrite
 from sys import argv
 
 input_path = argv[1]
-output_path = '../../assets/player.png'
+image_name = argv[2]
+output_path = f'../../assets/{image_name}.png'
 
 input = imread(input_path)
 output = remove(input)

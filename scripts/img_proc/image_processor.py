9bf0bdcfe8b5f3d03bb9db21a24c197838ffe709
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-04-08 22:10:01 +0545

    PP-22 Change background removal algorithm

--- scripts/img_proc/image_processor.py
+++ scripts/img_proc/image_processor.py
@@ -1,42 +1,23 @@
 import cv2
 import numpy as np
-from rembg import remove
-from PIL import Image
-from io import BytesIO
-from sys import argv
-import os
+import sys 
+from skimage import filters
+ 
+input = sys.argv[1]
+image = cv2.imread(input);
+image_rgba = cv2.cvtColor(image, cv2.COLOR_RGB2RGBA)
+image_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+cv2.imshow('gray image', image_gray)
+cv2.waitKey(0)
 
+threshold = filters.threshold_otsu(filters.sobel(image_gray))
 
-input_path = argv[1]
-image_name = argv[2]
-output_path = f'../../assets/{image_name}.png'
+fudge_factor = 0.3
+image_bw = filters.sobel(image_gray) > (threshold * fudge_factor)
+image_bw = np.uint8(image_bw * 255)
+cv2.imshow('sobel image', image_bw)
+cv2.waitKey(0)
 
+image_rgba[:, :, 3] = image_bw
 
-def remove_white_background(image_path, output_path):
-    img = cv2.imread(image_path)
-    img_cv = cv2.cvtColor(img, cv2.COLOR_RGBA2BGRA)
-
-    lower_white = np.array([100, 100, 100, 0])
-    upper_white = np.array([255, 255, 255, 255])
-
-    mask = cv2.inRange(img_cv, lower_white, upper_white)
-
-    img_cv[:, :, 3] = np.where(mask == 255, 0, img_cv[:, :, 3])
-
-    cv2.imwrite(output_path, img_cv)
-
-
-def resize_image(image_path, width, height):
-    image = cv2.imread(image_path)
-    resized_image = cv2.resize(image, (width, height))
-    cv2.imwrite(image_path, resized_image)
-
-
-def main():
-    if image_name == 'Platform':
-        resize_image(input_path, 1920, 1200)
-    remove_white_background(input_path, output_path)
-
-
-if __name__ == "__main__":
-    main()
+cv2.imwrite('output.png', image_rgba)

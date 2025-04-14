3692b05fdfc8f5c7c3166ae72be443466b04dfbe
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-04-14 12:36:19 +0545

    PP-73 Remove image processing previews

--- scripts/img_proc/image_processor.py
+++ scripts/img_proc/image_processor.py
@@ -10,8 +10,6 @@ image = cv2.imread(sys.argv[1]);
 
 # Gray scale conversion
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow('gray image', image_gray)
-cv2.waitKey(0)
 
 # Sobel edge detection
 sobel = filters.sobel(image_gray)
@@ -19,21 +17,15 @@ threshold = filters.threshold_otsu(sobel)
 fudge_factor = 0.1
 edge_mask = sobel > (threshold * fudge_factor)
 edge_mask = (edge_mask * 255).astype(np.uint8)
-cv2.imshow('sobel image', edge_mask)
-cv2.waitKey(0)
 
 # Dilate the edges
 kernel_v = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
 kernel_h = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 1))
 dilated = cv2.dilate(edge_mask, kernel_v)
 dilated = cv2.dilate(dilated, kernel_h)
-cv2.imshow('sobel image dilated', dilated)
-cv2.waitKey(0)
 
 # Fill internal holes
 filled = binary_fill_holes(dilated > 0).astype(np.uint8) * 255
-cv2.imshow('sobel image filled', filled)
-cv2.waitKey(0)
 
 # Add alpha channel
 image_rgba = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)

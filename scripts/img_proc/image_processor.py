865d2aff4c2f2d62f1625e4d1707073a73a9bdf3
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-04-20 21:17:01 +0545

    PP-73 Modularize image_processor.py

--- scripts/img_proc/image_processor.py
+++ scripts/img_proc/image_processor.py
@@ -6,49 +6,59 @@ from scipy.ndimage import binary_fill_holes
 from os import path 
 
 
-if len(sys.argv) != 3:
-    print(f'{len(sys.argv)} arguments provided. Expected 2 arguments.')
-    sys.exit(1)
+def _remove_background(image: np.ndarray) -> np.ndarray:
+    # Gray scale conversion
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-output_dir = f'{path.dirname(__file__)}/../../assets'
-if not path.exists(output_dir):
-    print(f'Output directory {output_dir} does not exist.')
-    sys.exit(1)
+    # Sobel edge detection
+    sobel = filters.sobel(image_gray)
 
-output = f'{output_dir}/{sys.argv[2]}.png'
+    threshold = filters.threshold_otsu(sobel)
+    fudge_factor = 0.1
+    edge_mask = sobel > (threshold * fudge_factor)
+    edge_mask = (edge_mask * 255).astype(np.uint8)
 
-image = cv2.imread(sys.argv[1]);
-if image is None:
-    print(f'Could not read image {sys.argv[1]}')
-    sys.exit(1)
+    # Dilate the edges
+    kernel_v = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
+    kernel_h = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 1))
+    dilated = cv2.dilate(edge_mask, kernel_v)
+    dilated = cv2.dilate(dilated, kernel_h)
 
-print(f'Processing image {sys.argv[1]}...')
+    # Fill internal holes
+    filled = binary_fill_holes(dilated > 0).astype(np.uint8) * 255
 
-# Gray scale conversion
-image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Add alpha channel
+    image_rgba = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
+    image_rgba[:, :, 3] = filled 
 
-# Sobel edge detection
-sobel = filters.sobel(image_gray)
+    return image_rgba
 
-threshold = filters.threshold_otsu(sobel)
-fudge_factor = 0.1
-edge_mask = sobel > (threshold * fudge_factor)
-edge_mask = (edge_mask * 255).astype(np.uint8)
 
-# Dilate the edges
-kernel_v = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
-kernel_h = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 1))
-dilated = cv2.dilate(edge_mask, kernel_v)
-dilated = cv2.dilate(dilated, kernel_h)
+def _main() -> None:
+    if len(sys.argv) != 3:
+        print(f'{len(sys.argv)} arguments provided. Expected 2 arguments.')
+        sys.exit(1)
 
-# Fill internal holes
-filled = binary_fill_holes(dilated > 0).astype(np.uint8) * 255
+    output_dir = f'{path.dirname(__file__)}/../../assets'
+    if not path.exists(output_dir):
+        print(f'Output directory {output_dir} does not exist.')
+        sys.exit(1)
 
-# Add alpha channel
-image_rgba = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
-image_rgba[:, :, 3] = filled 
+    output = f'{output_dir}/{sys.argv[2]}.png'
 
-if not cv2.imwrite(output, image_rgba):
-    print(f'Failed to write image to {output}')
-    sys.exit(1)
-print(f'Image saved to {output}')
+    image = cv2.imread(sys.argv[1]);
+    if image is None:
+        print(f'Could not read image {sys.argv[1]}')
+        sys.exit(1)
+
+    print(f'Processing image {sys.argv[1]}...')
+    background_removed_image = remove_background(image)
+
+    if not cv2.imwrite(output, background_removed_image):
+        print(f'Failed to write image to {output}')
+        sys.exit(1)
+    print(f'Image saved to {output}')
+
+
+if __name__ == '__main__':
+    _main()

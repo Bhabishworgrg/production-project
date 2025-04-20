58a2f3c37862c9b0bcc05f9ca22b8e3b162ac117
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-04-20 22:29:38 +0545

    PP-74 Add color thresholding algorithm and make it choosable

--- scripts/img_proc/image_processor.py
+++ scripts/img_proc/image_processor.py
@@ -3,40 +3,55 @@ import numpy as np
 import sys 
 from skimage import filters
 from scipy.ndimage import binary_fill_holes
-from os import path 
+from os import path
 
 
-def _remove_background(image: np.ndarray) -> np.ndarray:
-    # Gray scale conversion
-    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def _remove_background(image: np.ndarray, algorithm: str) -> np.ndarray:
+    if algorithm == 'color_threshold':
+        image_rgba = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
 
-    # Sobel edge detection
-    sobel = filters.sobel(image_gray)
+        lower_white = np.array([200, 200, 200, 0])
+        upper_white = np.array([255, 255, 255, 255])
 
-    threshold = filters.threshold_otsu(sobel)
-    fudge_factor = 0.1
-    edge_mask = sobel > (threshold * fudge_factor)
-    edge_mask = (edge_mask * 255).astype(np.uint8)
+        mask = cv2.inRange(image_rgba, lower_white, upper_white)
+        image_rgba[:, :, 3] = np.where(mask == 255, 0, image_rgba[:, :, 3])
+        
+        return image_rgba
+    
+    if algorithm == 'edge_detection':
+        # Gray scale conversion
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Dilate the edges
-    kernel_v = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
-    kernel_h = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 1))
-    dilated = cv2.dilate(edge_mask, kernel_v)
-    dilated = cv2.dilate(dilated, kernel_h)
+        # Sobel edge detection
+        sobel = filters.sobel(image_gray)
 
-    # Fill internal holes
-    filled = binary_fill_holes(dilated > 0).astype(np.uint8) * 255
+        threshold = filters.threshold_otsu(sobel)
+        fudge_factor = 0.1
+        edge_mask = sobel > (threshold * fudge_factor)
+        edge_mask = (edge_mask * 255).astype(np.uint8)
 
-    # Add alpha channel
-    image_rgba = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
-    image_rgba[:, :, 3] = filled 
+        # Dilate the edges
+        kernel_v = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 3))
+        kernel_h = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 1))
+        dilated = cv2.dilate(edge_mask, kernel_v)
+        dilated = cv2.dilate(dilated, kernel_h)
 
-    return image_rgba
+        # Fill internal holes
+        filled = binary_fill_holes(dilated > 0).astype(np.uint8) * 255
+
+        # Add alpha channel
+        image_rgba = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
+        image_rgba[:, :, 3] = filled 
+
+        return image_rgba
+
+    print(f'Unknown algorithm {algorithm}. Supported algorithms: color_threshold, edge_detection')
+    return None
 
 
 def _main() -> None:
-    if len(sys.argv) != 3:
-        print(f'{len(sys.argv)} arguments provided. Expected 2 arguments.')
+    if len(sys.argv) != 4:
+        print(f'{len(sys.argv)} arguments provided. Expected 3 arguments.')
         sys.exit(1)
 
     output_dir = f'{path.dirname(__file__)}/../../assets'
@@ -52,7 +67,7 @@ def _main() -> None:
         sys.exit(1)
 
     print(f'Processing image {sys.argv[1]}...')
-    background_removed_image = remove_background(image)
+    background_removed_image = _remove_background(image, sys.argv[3])
 
     if not cv2.imwrite(output, background_removed_image):
         print(f'Failed to write image to {output}')

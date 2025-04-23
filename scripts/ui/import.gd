3ab28c5f51d1be1435a897d276973326a444e7f3
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-04-23 21:35:32 +0545

    PP-74 Allow keyboard navigating in import screen

--- scripts/ui/import.gd
+++ scripts/ui/import.gd
@@ -1,6 +1,14 @@
 extends Control
 
 
+@export var initial_focus_control: Control
+
+
+func _ready() -> void:
+	if initial_focus_control:
+		initial_focus_control.grab_focus()
+
+
 func _on_button_pressed() -> void:
 	$FileDialog.show()
 

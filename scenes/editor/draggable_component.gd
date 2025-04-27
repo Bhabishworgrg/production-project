b5d1eaadf820273a077bf3652011d6ba803cb259
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-04-27 21:39:37 +0545

    PP-64 Segregate draggable functionality to a component

---
+++ scenes/editor/draggable_component.gd
@@ -0,0 +1,29 @@
+extends Node 
+
+
+var draggable: bool
+var parent: CollisionObject2D 
+
+
+func _ready() -> void:
+	draggable = false
+	parent = get_parent()
+
+	parent.mouse_entered.connect(_on_mouse_entered)
+	parent.mouse_exited.connect(_on_mouse_exited)
+
+
+func _process(delta: float) -> void:
+	if draggable:
+		if Input.is_action_pressed('left_click'):
+			parent.global_position = parent.get_global_mouse_position()
+	
+
+func _on_mouse_entered() -> void:
+	draggable = true
+	parent.scale = Vector2(1.05, 1.05)
+
+
+func _on_mouse_exited() -> void:
+	draggable = false
+	parent.scale = Vector2(1, 1)

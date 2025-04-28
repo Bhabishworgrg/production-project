e3bda4f701f9cc920e5d86903f7dad64507c4bcb
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-04-28 23:47:25 +0545

    PP-41 Set background in editor

--- scripts/editor/config_window.gd
+++ scripts/editor/config_window.gd
@@ -1,5 +1,24 @@
 extends Window
 
+class_name ConfigWindow
+
+
+@export
+var _background_path_field: LineEdit
+@export
+var _music_path_field: LineEdit
+@export
+var _background: TextureRect 
+
 
 func _on_close_requested() -> void:
 	hide()	
+
+
+func _on_save_button_pressed() -> void:
+	var background_path = _background_path_field.text
+	var music_path = _music_path_field.text
+	
+	_background.texture = load(background_path)
+
+	hide()

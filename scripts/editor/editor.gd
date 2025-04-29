58e2c41b9d697244ceeaaaad40517014b8a6f63f
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-04-29 23:06:56 +0545

    PP-51 Implement saving data dictionary as binary

--- scripts/editor/editor.gd
+++ scripts/editor/editor.gd
@@ -14,3 +14,19 @@ func _on_play_button_pressed() -> void:
 
 func _process(_delta: float) -> void:
 	print($Viewport/Player.position)
+
+
+func _on_save_button_pressed() -> void:
+	GlobalState.set_value("player", "position", $Viewport/Player.position)
+	GlobalState.set_value("area_completed", "position", $Viewport/AreaCompleted.position)
+
+	var dir_access = DirAccess.open("res://")
+	if dir_access.change_dir("save") != OK:
+		dir_access.make_dir("save")
+
+	var save_file = FileAccess.open("res://save/savegame.save", FileAccess.WRITE)
+	if save_file:
+		save_file.store_var(GlobalState.get_all())
+		save_file.close()
+	else:
+		print_rich('[color=red]ERROR[/color]: Failed to save file.')

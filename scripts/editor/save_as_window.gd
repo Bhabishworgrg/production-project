c749937aada382ed0b28e8ba9a248c08c583a8a6
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-05-06 23:30:43 +0545

    PP-51 Create Save As window

---
+++ scripts/editor/save_as_window.gd
@@ -0,0 +1,30 @@
+extends Window
+
+
+@export
+var _player: Node2D 
+@export
+var _area_completed: Node2D
+
+
+func _on_close_requested() -> void:
+	hide()
+
+
+func _on_save_button_pressed() -> void:
+	GlobalState.set_value("player", "position", _player.position)
+	GlobalState.set_value("area_completed", "position", _area_completed.position)
+
+	var dir_access = DirAccess.open("res://")
+	if dir_access.change_dir("save") != OK:
+		dir_access.make_dir("save")
+
+	var file_name = $PathField.text
+	var save_file = FileAccess.open("res://save".path_join(file_name)+".save", FileAccess.WRITE)
+	if save_file:
+		save_file.store_var(GlobalState.get_all())
+		save_file.close()
+	else:
+		print_rich('[color=red]ERROR[/color]: Failed to save file.')
+	
+	hide()

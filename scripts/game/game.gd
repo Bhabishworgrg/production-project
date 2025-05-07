62ce05e11a4828a9b9ea075ea1e4467110e62a36
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-05-07 18:44:45 +0545

    PP-75 Handle invalid backgrounds and music

--- scripts/game/game.gd
+++ scripts/game/game.gd
@@ -2,6 +2,6 @@ extends Node2D
 
 
 func _ready() -> void:
-	$Background.texture = GlobalState.get_value("background", "texture")
+	$Background.texture = load(GlobalState.get_value("background", "texture"))
 	$MusicPlayer.stream = load(GlobalState.get_value("music", "audio_stream"))
 	$MusicPlayer.play()

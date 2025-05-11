313a2d93d46739a62c7887bd2aca60e8cdf5b4b8
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-05-11 22:29:38 +0545

    Create pause menu

---
+++ scripts/ui/game_stop_screen.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed('escape'):
+		show()
+
+
+func _on_replay_button_pressed() -> void:
+	var current_scene = GlobalState.get_current_scene()
+	get_tree().change_scene_to_file.call_deferred(current_scene)
+
+
+func _on_exit_button_pressed() -> void:
+	$ConfirmationDialog.show()
+
+
+func _on_confirmation_dialog_confirmed() -> void:
+	get_tree().quit()
+
+
+func _on_unpause_button_pressed() -> void:
+	get_tree().paused = false
+	hide()
---
+++ scripts/ui/pause_menu.gd
@@ -0,0 +1,25 @@
+extends CanvasLayer
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed('escape'):
+		get_tree().paused = true
+		show()
+
+
+func _on_exit_button_pressed() -> void:
+	$ConfirmationDialog.show()
+
+
+func _on_confirmation_dialog_confirmed() -> void:
+	get_tree().quit()
+
+
+func _on_go_back_button_pressed() -> void:
+	var current_scene = GlobalState.get_current_scene()
+	get_tree().change_scene_to_file.call_deferred(current_scene)
+
+
+func _on_unpause_button_pressed() -> void:
+	get_tree().paused = false
+	hide()
